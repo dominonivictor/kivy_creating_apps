85c4e2fcbda70e0b708a69899d2d296c864626af
Victor Dominoni <dominoni.victor@gmail.com>
2020-01-21 21:52:30 -0300

    chore: chapter 1, the basics

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+
+class SearchScreen(Screen):
+    pass
+
+class WeatherApp(App):
+    pass
+
+if __name__ == '__main__':
+    WeatherApp().run()
